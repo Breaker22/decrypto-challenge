48f9f6db6dd4373e5758fea8cca8c876fb04eca1
Breaker22 <gastyg16@gmail.com>
2024-06-01 17:54:58 -0300

    Add init config

---
+++ src/main/java/ar/com/decrypto/challenge/DecryptoChallengeApplication.java
@@ -0,0 +1,13 @@
+package ar.com.decrypto.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecryptoChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecryptoChallengeApplication.class, args);
+	}
+
+}
